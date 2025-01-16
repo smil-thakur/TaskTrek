e8ab55af0287ebb0e50b71dab688d908a27717c8
smil-thakur <smil.raj.thakur@gmail.com>
2025-01-17 00:02:25 +0530

    material 19 design upgrade

--- TaskTrek.FE/TaskTrek/src/app/home-module/main-page/main-page.component.ts
+++ TaskTrek.FE/TaskTrek/src/app/home-module/main-page/main-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonComponentsModule } from '../../common-components/common-components.module';
 import { MatTabBody, MatTab, MatTabHeader, MatTabContent, MatTabGroup } from "@angular/material/tabs"
 import { TicketWindowComponent } from '../ticket-window/ticket-window.component';
-import { MatButton } from '@angular/material/button';
 
 @Component({
     selector: 'app-main-page',
@@ -11,7 +10,6 @@ import { MatButton } from '@angular/material/button';
         MatTabGroup,
         MatTab,
         TicketWindowComponent,
-        MatButton
     ],
     templateUrl: './main-page.component.html',
     styleUrl: './main-page.component.scss'
--- TaskTrek.FE/TaskTrek/src/app/login-page/login-page.component.ts
+++ TaskTrek.FE/TaskTrek/src/app/login-page/login-page.component.ts
@@ -1,26 +1,21 @@
 import { Component } from '@angular/core';
 import { MatCard } from '@angular/material/card'
-import { MatDivider } from "@angular/material/divider"
 import { MatFormField, MatLabel, MatHint } from "@angular/material/form-field"
-import { MatInput } from "@angular/material/input"
 import { LogoComponent } from '../logo/logo.component';
 import { MatButton } from "@angular/material/button"
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-login-page',
-    imports: [
-        MatCard,
-        MatDivider,
-        MatFormField,
-        MatLabel,
-        MatHint,
-        MatInput,
-        LogoComponent,
-        MatButton
-    ],
-    templateUrl: './login-page.component.html',
-    styleUrl: './login-page.component.scss'
+  selector: 'app-login-page',
+  imports: [
+    MatCard,
+    MatFormField,
+    MatLabel,
+    LogoComponent,
+    MatButton
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
 
