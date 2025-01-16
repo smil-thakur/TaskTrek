4d5a7436125814b26f75d8925d8b5ec9a205a604
smil-thakur <smil.raj.thakur@gmail.com>
2025-01-17 00:10:27 +0530

    vscode error fix

--- TaskTrek.FE/TaskTrek/src/app/common-components/header/header.component.ts
+++ TaskTrek.FE/TaskTrek/src/app/common-components/header/header.component.ts
@@ -3,18 +3,19 @@ import { LogoComponent } from '../../logo/logo.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from "@angular/material/icon"
 import { MatTooltipModule } from "@angular/material/tooltip"
-import { MatDialogRef, MatDialog } from "@angular/material/dialog"
+import { MatDialog } from "@angular/material/dialog"
 import { NewTicketDialogComponent } from '../../dialogs/new-ticket-dialog/new-ticket-dialog.component';
 @Component({
-    selector: 'app-header',
-    imports: [
-        LogoComponent,
-        MatButtonModule,
-        MatIconModule,
-        MatTooltipModule,
-    ],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss'
+  selector: 'app-header',
+  imports: [
+    LogoComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  standalone: true
 })
 export class HeaderComponent {
 
--- TaskTrek.FE/TaskTrek/src/app/logo/logo.component.ts
+++ TaskTrek.FE/TaskTrek/src/app/logo/logo.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-logo',
-    imports: [],
-    templateUrl: './logo.component.html',
-    styleUrl: './logo.component.scss'
+  selector: 'app-logo',
+  imports: [],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss',
+  standalone: true
 })
 export class LogoComponent {
   @Input() width!: number;
